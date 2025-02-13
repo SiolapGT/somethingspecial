67ed576ebfae59ed4ca220b404a5761ae37ad869
SiolapGT <98896120+SiolapGT@users.noreply.github.com>
2025-02-13 11:18:28 +0700

    Update button.js

--- button.js
+++ button.js
@@ -1,14 +1,14 @@
 const messages = [
-    "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
+    "Yang benerr?",
+    "Eh beneran ini??",
+    "Dhirrr?",
+    "Dhir please...",
+    "Coba pikirin ulang dehh!",
     "If you say no, I will be really sad...",
     "I will be very sad...",
     "I will be very very very sad...",
     "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Just kidding, say yes please!"
 ];
 
 let messageIndex = 0;
